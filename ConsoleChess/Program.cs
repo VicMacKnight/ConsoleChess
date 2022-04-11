90292673975948960e0106f905dfc57231eebafc
vic <vfn.franco@hotmail.com>
2022-04-11 16:15:26 -0300

    atualização

--- ConsoleChess/Program.cs
+++ ConsoleChess/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using tabuleiro;
+
+namespace ConsoleChess
+{
+    public class Program
+    {
+        static void Main(string[] args) {
+
+            Posicao P = new Posicao(3, 4);
+        }
+    }
+}
